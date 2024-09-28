594009a11aab46a5cf2811745cc1f6d6b5709dbf
HARSHAN A M <harshanmathi06@gmail.com>
2024-09-29 01:11:21 +0530

    feat: docker

--- database.py
+++ database.py
@@ -8,8 +8,13 @@ uri = os.getenv("DB_URL")
 
 client = MongoClient(uri)
 
+db=None
+
 try:
     db=client[os.getenv("DB_NAME")]
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+if db is None:
+    raise ValueError("Database connection was not established.")
